ef2cb8dbe56153c9b6d75d645008bb92d5ce91d2
zhouziyang <3241917108@qq.com>
2024-10-23 13:52:02 +0800

    完善会员等级功能 完善会员模块路由 完善文件上传OSS文件保存路径

--- zzy-third-party/src/main/java/com/zzy/mall/third/controller/OSSController.java
+++ zzy-third-party/src/main/java/com/zzy/mall/third/controller/OSSController.java
@@ -37,7 +37,7 @@ public class OSSController {
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
         String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        String dir = format + "/"; // 用户上传文件时指定的前缀。
+        String dir = format; // 用户上传文件时指定的前缀。
 
         // 创建OSSClient实例。
         //OSS ossClient = new OSSClientBuilder().build(endpoint, accessId, accessKey);
